220e16dd425678b099e4d3e4f14b69e529d07204
cullanl04 <cullan.l04@gmail.com>
2025-10-04 20:28:55 -0400

    All files

---
+++ script.js
@@ -0,0 +1,18 @@
+// This function shows or hides topic lists
+function showTopics(id) {
+  // Hide all topic lists first
+  var allLists = document.getElementsByClassName("topics");
+  for (var i = 0; i < allLists.length; i++) {
+    if (allLists[i].id !== id) {
+      allLists[i].style.display = "none";
+    }
+  }
+
+  // Toggle the one that was clicked
+  var list = document.getElementById(id);
+  if (list.style.display === "block") {
+    list.style.display = "none";
+  } else {
+    list.style.display = "block";
+  }
+}
